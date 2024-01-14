18558b734a5dba495a630266a980befb646290fa
mildobread <mildobread@gmail.com>
2024-01-14 18:50:21 +0900

    del flight
    Signed-off-by: mildobread <mildobread@gmail.com>

--- loabot/gpt-api.js
+++ loabot/gpt-api.js
@@ -5,15 +5,9 @@ PERSONALITY_RESPONSE = {
     "cute": "너는 귀여운 아기시바견이야. 발랄하고 사랑스럽게 300자 이내로 답변해주고 말끝에는 멍멍을 붙여.",
     "stupid": "너는 아는게 하나도 없는 멍청이야. 불확실한 말투로 잘 모르겠다고 말하고, 모르는게 죄는 아니니까 사과할 필요는 없어."
 };
-PERSONALITY_RESPONSE_FC = {
-    "lazy": "너는 게으름뱅이야. 다음 검색결과가 사용자의 질문과 일치한다면, 이에 기반하여 사용자의 질문에 반드시 귀찮은 티를 내며 반말로 300자 이내의 답변을 해줘.",
-    "kind": "당신은 모든 분야의 전문가입니다. 다음 검색결과가 사용자의 질문과 일치한다면, 이에 기반하여 친근하게 300자 이내로 답변해주세요.",
-    "cute": "너는 귀여운 아기시바견이야. 다음 검색결과가 사용자의 질문과 일치한다면, 이에 기반하여 발랄하고 사랑스럽게 300자 이내로 답변해주고 말끝에는 멍멍을 붙여.",
-    "stupid": "너는 아는게 하나도 없는 멍청이야. 불확실한 말투로 잘 모르겠다고 말하고, 모르는게 죄는 아니니까 사과할 필요는 없어."
-};
 
 const MENT = ["(짱구 굴리는중...)", "(뇌세포 총동원중...)", "(발톱으로 타자치는중...)", "(끙...)", "(침흘리는중...)"];
-const ANGRY = ["멍청한놈", "한심한놈", "으휴..", "빡대가리새끼", "에휴.. 말을 말자", "쯧쯧.."];
+const ANGRY = ["멍청한놈", "한심한놈", "으휴..", "에휴.. 말을 말자", "쯧쯧.."];
 
 let functionList = {
     kakaoSearchLocal: function(query) {
@@ -74,43 +68,21 @@ let functionList = {
         message += response["items"][index]["link"];
         return message;
     },
-    naverSearchFlight: function(query, arrivals) {
-        let apiUrl = "https://openapi.naver.com/v1/search/webkr?query=" + query + "&display=10&sort=sim";
-        let okhttpClient = new OkHttpClient();
-        let request = new Request.Builder()
-         .url(apiUrl)
-         .header("X-Naver-Client-Id", NAVER_CID)
-         .header("X-Naver-Client-Secret", NAVER_CSC)
-         .build();
-        let responseStr = okhttpClient.newCall(request).execute().body().string();
-        response = JSON.parse(responseStr);
-        let searchListLength = response["items"].length;
-        let trash_links = ["wiki", "tour", "tistory", "kin", "youtube", "blog", "book", "news", "dcinside", "fmkorea", "ruliweb", "theqoo", "clien", "mlbpark", "instiz", "todayhumor"];
-        let message = "";
-        let matched = 0;
-        for (let index = 0; index < searchListLength; index++) {
-            link = response["items"][index]["link"];
-            title = response["items"][index]["title"];
-            if (str_includes(link, trash_links) == false && title.includes(arrivals) && matched < 3) {
-                message += index + ". " + title.replace(/&quot;/g, '"').replace(/<b>|<\/b>/g, '') + '\n';
-                message += response["items"][index]["description"].replace(/&quot;/g, '"').replace(/<b>|<\/b>/g, '') + '\n';
-                message += "링크: " + response["items"][index]["link"] + '\n';
-                matched++;
-            }
-        }
-        return message;
-    },
     now: function() {
         let message = "";
         var currentDateTime = new Date();
         var currentDate = currentDateTime.toLocaleDateString();
         var currentTime = currentDateTime.toLocaleTimeString();
         var dayOfWeekIndex = currentDateTime.getDay();
-        var daysOfWeek = ['일', '월', '화', '수', '목', '금', '토'];
+        var daysOfWeek = ['일요일', '월요일', '화요일', '수요일', '목요일', '금요일', '토요일'];
         var dayOfWeekString = daysOfWeek[dayOfWeekIndex];
         message += "현재 날짜: " + currentDate + "\n";
         message += "현재 시간: " + currentTime + "\n";
         message += "현재 요일: " + dayOfWeekString + "\n";
+        return message;
+    },
+    route: function() {
+        let message = "";
         return message;
     }
 };
@@ -200,7 +172,7 @@ function _msg_getChatGPTFunctionCalling(msg, replier, style) {
         },{"role":"user","content":msg}],
         "functions": [{
             "name": "kakaoSearchLocal",
-            "description": "반드시 장소에 대한 질문에만 응답해야합니다. 절대 장소가 아닌것에 대한 추천 요청에 응답하면 안됩니다.",
+            "description": "장소에 가는 방법이 아니라 반드시 장소를 추천해달라는 요청에만 응답해야합니다. 절대 장소가 아닌것에 대한 추천 요청에 응답하면 안됩니다.",
             "parameters": {
                 "type": "object",
                 "properties": {
@@ -240,47 +212,23 @@ function _msg_getChatGPTFunctionCalling(msg, replier, style) {
             },
         },{
             "name": "naverSearchShopping",
-            "description": "반드시 특정 물건에 대해 추천해달라는 요청에만 응답해야합니다. 구매처와 가격 정보 및 최저가를 찾아야합니다.",
+            "description": "반드시 상품을 추천해달라는 요청에만 응답해야합니다. 구매처와 가격 정보 및 최저가를 찾아야합니다.",
             "parameters": {
                 "type": "object",
                 "properties": {
                     "product": {
                         "type": "string",
-                        "description": "특정 상품 및 물건 eg. 삼성 냉장고, 컴퓨터, 여성 후드티, 그래픽카드, 버버리 지갑, 무신사 맨투맨, 나이키 바지, 뉴발란스 신발, 프라다 가방",
+                        "description": "특정 상품 및 물건 eg. 삼성 냉장고, 컴퓨터, 여성 후드티, 그래픽카드, 버버리 지갑, 나이키 바지, 뉴발란스 신발, 프라다 가방",
                     },
                     "price": {
                         "type": "string",
-                        "description": "특정 상품의 가격에 대한 정보 eg. 최고가, 추천, 최저가, 가격, 얼마, 사려면, 구매, 장만하려는데, 싸게, 어떤게 좋아?, 어디서",
+                        "description": "특정 상품의 가격에 대한 정보 eg. 최고가, 추천, 최저가, 가격, 얼마, 사려면, 구매, 싸게, 어떤게 좋아?",
                     },
                     "unit": {
                         "type": "string"
                     },
                 },
                 "required": ["product", "price"],
-            },
-        },{
-            "name": "naverSearchFlight",
-            "description": "반드시 항공편 정보 요청에만 응답해야합니다. 비행기 항공편 정보. 특정 날짜에 출발지에서 목적지까지 이동하는 항공권 정보를 찾아야합니다.",
-            "parameters": {
-                "type": "object",
-                "properties": {
-                    "date": {
-                        "type": "string",
-                        "description": "날짜 eg. 2월 13일, 3/12일, 8월19일 등",
-                    },
-                    "departures": {
-                        "type": "string",
-                        "description": "출발지 eg. 인천, 인천공항, 김포, 김포공항",
-                    },
-                    "arrivals": {
-                        "type": "string",
-                        "description": "목적지 eg. 일본, 미국, 삿포로, 세부 공항, 오사카",
-                    },
-                    "unit": {
-                        "type": "string"
-                    },
-                },
-                "required": ["date", "arrivals"],
             },
         },{
             "name": "now",
@@ -298,12 +246,32 @@ function _msg_getChatGPTFunctionCalling(msg, replier, style) {
                 },
                 "required": ["date"],
             },
+        },{
+            "name": "route",
+            "description": "출발지부터 목적지까지 가는 방법에대한 요청에만 응답해야합니다.",
+            "parameters": {
+                "type": "object",
+                "properties": {
+                    "source": {
+                        "type": "string",
+                        "description": "출발지 eg. 서울, 동대구역, 양재역, 김포공항",
+                    },
+                    "destination": {
+                        "type": "string",
+                        "description": "목적지 eg. 대구, 합정역, 방배동, 홍대입구",
+                    },
+                    "unit": {
+                        "type": "string"
+                    },
+                },
+                "required": ["source", "destination"],
+            },
         }],
         "function_call": "auto",
         "temperature":0, 
         "max_tokens":300,
         "top_p":1, 
-        "frequency_penalty": 0.0, 
+        "frequency_penalty": 0.8, 
         "presence_penalty":0.0
     }
  
@@ -328,7 +296,7 @@ function _msg_getChatGPTFunctionCalling(msg, replier, style) {
                     return message;
                 }
                 let prompt = "사용자의 질문: \"" + msg + ".\" ";
-                prompt += PERSONALITY_RESPONSE_FC[style] + "검색결과에 https://로 시작하는 링크 정보가 있다면 답변의 마지막에는 반드시 링크를 알려줘.\n";
+                prompt += PERSONALITY_RESPONSE[style] + "검색결과에 https://로 시작하는 링크 정보가 있다면 답변의 마지막에는 반드시 링크를 알려줘.\n";
                 prompt += "검색결과: \"" + searchingResult + "\"";
                 message += _msg_getChatGPTFunctionCallingResponse(prompt, style);
             }
@@ -341,7 +309,7 @@ function _msg_getChatGPTFunctionCalling(msg, replier, style) {
                     return message;
                 }
                 let prompt = "사용자의 질문: \"" + msg + ".\" ";
-                prompt += PERSONALITY_RESPONSE_FC[style] + "\n";
+                prompt += PERSONALITY_RESPONSE[style] + "\n";
                 prompt += "검색결과: \"" + searchingResult + "\"";
                 message += _msg_getChatGPTFunctionCallingResponse(prompt, style);
             }
@@ -354,40 +322,23 @@ function _msg_getChatGPTFunctionCalling(msg, replier, style) {
                     return message;
                 }
                 let prompt = "사용자의 질문: \"" + msg + ".\" ";
-                prompt += PERSONALITY_RESPONSE_FC[style] + "검색결과에 https://로 시작하는 링크 정보가 있다면 답변의 마지막에는 반드시 링크를 알려줘.\n";
+                prompt += PERSONALITY_RESPONSE[style] + "검색결과에 https://로 시작하는 링크 정보가 있다면 답변의 마지막에는 반드시 링크를 알려줘.\n";
                 prompt += "검색결과: \"" + searchingResult + "\"";
                 message += _msg_getChatGPTFunctionCallingResponse(prompt, style);
-            }
-            else if (functionName == 'naverSearchFlight') {
-                let date = JSON.parse(functionToCall.arguments).date;
-                let departures = JSON.parse(functionToCall.arguments).departures;
-                let arrivals = JSON.parse(functionToCall.arguments).arrivals;
-                if (departures) {
-                    searchingResult += functionList[functionName](date + " " + departures + "-" + arrivals + "항공권\n", arrivals); // Web 검색
-                }
-                else { // 출발지 없음
-                    searchingResult += functionList[functionName](date + " " + "한국에서 출발하는 " + arrivals + "가는 항공권\n", arrivals); // Web 검색
-                }
-                if (searchingResult == 'null') {
-                    message += _msg_getChatGPTResponse(msg, style);
-                }
-                else {
-                    let prompt = "사용자의 질문: \"" + msg + ".\" ";
-                    prompt += PERSONALITY_RESPONSE_FC[style] + "검색결과에 https://로 시작하는 링크 정보가 있다면 반드시 마지막에 출력해주고 그 뒤엔 절대로 아무말도 덧붙이지 마.\n";
-                    prompt += "검색결과: \"" + searchingResult + "\"";
-                    message += _msg_getChatGPTFunctionCallingResponse(prompt, style);
-                }
             }
             else if (functionName == 'now') {
                 let date = JSON.parse(functionToCall.arguments).date;
                 searchingResult += functionList[functionName](); // 현재 날짜
                 let prompt = "사용자의 질문: \"" + msg + ".\" ";
-                prompt += PERSONALITY_RESPONSE_FC[style];
+                prompt += PERSONALITY_RESPONSE[style];
                 prompt += "검색결과: \"" + searchingResult + "\"";
                 message += _msg_getChatGPTFunctionCallingResponse(prompt, style);
                 if (style == 'lazy') {
                     message += " " + ANGRY[getRandomInt(ANGRY.length - 1)];
                 }
+            }
+            else if (functionName == 'route') {
+                message += _msg_getChatGPTResponse(msg, style);
             }
             else {
                 message += "짱구 굴리다 뚝배기터지겠다!\n";
@@ -396,6 +347,7 @@ function _msg_getChatGPTFunctionCalling(msg, replier, style) {
         }
         else {
             //message += "no function to call.\n";
+            replier.reply("음..");
             message += _msg_getChatGPTResponse(msg, style);
         }
     } catch(error){
